298652c4a301638fc2bb612a6c1f7857c92e84b4
Dave Sarbora, sam stoia and Young Liu <young@>
2019-04-02 11:51:35 -0700

    create a recipe box site.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from './models/recipe.model'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  recipes: Recipe[] = [
+    new Recipe('Sugar Cookies', ['flour', 'sugar', 'eggs', 'milk', 'butter'], ['mix ingredients', 'bake at 400 degrees for 10 minutes'], 'allrecipes', 'hard'),
+    new Recipe('Steak', ['salt', 'pepper', 'sirloin steak'], ['heat oil in pan', 'put steaks in heated pan', 'cook each side for 5 minutes'], 'allrecipes', 'medium'),
+    new Recipe('Oatmeal', ['oats', 'milk', 'sugar(optional)'], ['heat milk in pan', 'mix in oats', 'add sugar if desired'], 'allrecipes', 'easy')
+  ];
+
+  selectedRecipe: Recipe = null;
+  editRecipe(clickedRecipe) {
+    this.selectedRecipe=clickedRecipe;
+  }
+
+  finishedEditing() {
+    this.selectedRecipe = null;
+  }
+
+  difficultyColor(currentRecipe) {
+    if (currentRecipe.difficulty === 'easy') {
+      return 'bg-info';
+    } else if (currentRecipe.difficulty === 'medium'){
+      return 'bg-warning';
+    } else {
+      return 'bg-danger';
+    }
+  }
 }
---
+++ src/app/models/recipe.model.ts
@@ -0,0 +1,5 @@
+export class Recipe{
+  constructor(public title: string, public ingredients: string[], public instructions: string[], public source: string, public difficulty: string){}
+
+  
+}
